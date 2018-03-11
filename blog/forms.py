d4ebbf6114787288c57a32c31291c24cf8dd6827
aprilmao <aprilmao129@gmail.com>
2018-03-11 01:05:12 -0800

    Adding venv

--- blog/forms.py
+++ blog/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post
-from PIL import Image
 
 class PostForm(forms.ModelForm):
 

c7574465a52c0acd9e5bd6493df2d78398fbad7c
aprilmao <aprilmao129@gmail.com>
2018-03-11 01:21:45 -0800

    Pillow included

--- blog/forms.py
+++ blog/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post
-from PIL import Image
+from PIL import Image, ImageTk
 
 class PostForm(forms.ModelForm):
 
